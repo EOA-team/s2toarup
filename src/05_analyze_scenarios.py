892a7befec2a7bd0a811dbc08b7d711cfa9d4df4
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-10-26 16:55:54 +0200

    mapping of uncertainty scenarios

--- src/05_analyze_scenarios.py
+++ src/05_analyze_scenarios.py
@@ -1,5 +1,6 @@
 
 import glob
+import math
 import pandas as pd
 from pathlib import Path
 import geopandas as gpd
@@ -7,12 +8,18 @@ import rasterio as rio
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.ticker import FormatStrFormatter
+import matplotlib.ticker as ticker
 
 from agrisatpy.processing.extraction.utils import raster2table
 from agrisatpy.processing.extraction.sentinel2 import S2singlebands2table
 from copy import deepcopy
 
 
+plt.rcParams['xtick.labelsize'] = 11
+plt.rcParams['ytick.labelsize'] = 11
+
+plt.rcParams['axes.titlesize'] = 15
 gain_factor_refl = 0.01
 
 band_dict_l1c = {
@@ -32,8 +39,7 @@ band_dict_l1c = {
     },
     '60': {
         'B01': '*_B01.jp2',
-        'B09': '*_B09.jp2',
-        'B10': '*_B10.jp2'
+        'B09': '*_B09.jp2'
     }   
 }
 
@@ -267,7 +273,7 @@ def unc_boxplots(
         id_column: str,
         land_use_info: Path,
         out_dir: Path
-    ):
+    ) -> None:
     """
     Takes extracted ROI data (CSV files) and makes a box plot for each
     numerical variable (spectral bands, AOT and WV) taking into account
@@ -292,6 +298,10 @@ def unc_boxplots(
     l2a_scenarios = glob.glob(l2a_roi_scenarios.as_posix())
     l1c_original = glob.glob(l1c_original_data.as_posix())
     l2a_original = glob.glob(l2a_original_data.as_posix())
+    luc_info = gpd.read_file(land_use_info)
+
+    # save uncertainties per land cover class
+    uncertainties = []
 
     # loop over the different spatial resolutions and over the spectral
     # bands
@@ -305,6 +315,14 @@ def unc_boxplots(
 
         # number of ROIs
         rois = l1c_orig_df[id_column].unique()
+        roi_luc_mapping = dict.fromkeys(rois)
+        for roi in roi_luc_mapping:
+            roi_luc_mapping[roi] = luc_info[luc_info[id_column] == roi]['crop_type'].iloc[0]
+
+        # join with landuse information
+        l1c_orig_df = pd.merge(l1c_orig_df, luc_info, on=id_column)
+        l2a_orig_df = pd.merge(l2a_orig_df, luc_info, on=id_column)
+
         n_rois = len(rois)
 
         # group original data by ROIs
@@ -316,9 +334,11 @@ def unc_boxplots(
         n_bands = len(band_dict)
 
         # loop over single bands here
-        fig, axs = plt.subplots(n_bands, n_rois, constrained_layout=True)
+        fig, axs = plt.subplots(n_bands, n_rois, figsize=(35,40))
 
         for idx, band in enumerate(band_dict):
+
+            inner_band_dict = {'band': band}
 
             # read scenarios of the current band
             l1c_scenario_band = pd.read_csv(
@@ -328,14 +348,26 @@ def unc_boxplots(
                 [x for x in l2a_scenarios if Path(x).name.split('_')[1] == band][0]
             )
 
+            # join land use info
+            l1c_scenario_band = pd.merge(l1c_scenario_band, luc_info, on='fid')
+            l2a_scenario_band = pd.merge(l2a_scenario_band, luc_info, on='fid')
+
             # group by scenario and ROI to get the ROI mean per scenario
             l1c_grouped = l1c_scenario_band.groupby(by=['scenario', id_column]).mean()
             l2a_grouped = l2a_scenario_band.groupby(by=['scenario', id_column]).mean()
 
             # boxplot
             for jdx, roi in enumerate(rois):
+
                 l1c_grouped_roi = l1c_grouped.query(f'{id_column} == {roi}')
                 l2a_grouped_roi = l2a_grouped.query(f'{id_column} == {roi}')
+
+                # calculate standard uncertainty as variation coefficient
+                l1c_roi_unc = (np.std(l1c_grouped_roi[band]) / np.mean(l1c_grouped_roi[band])) * 1000
+                l2a_roi_unc = (np.std(l2a_grouped_roi[band]) / np.mean(l2a_grouped_roi[band])) * 1000
+
+                inner_band_dict[f'{roi_luc_mapping[roi]}_roi_L1C'] = l1c_roi_unc
+                inner_band_dict[f'{roi_luc_mapping[roi]}_roi_L2A'] = l2a_roi_unc
 
                 axs[idx][jdx].boxplot(
                     [
@@ -344,6 +376,8 @@ def unc_boxplots(
                     ]
                 )
                 axs[idx][jdx].set_xticklabels(['L1C', 'L2A'])
+                [t.set_color(i) for (i,t) in zip(['r','b'], axs[idx][jdx].xaxis.get_ticklabels())]
+
                 axs[idx][jdx].axhline(
                     l1c_orig_grouped[l1c_orig_grouped.index==roi][band].values * gain_factor_refl,
                     color='r',
@@ -354,35 +388,174 @@ def unc_boxplots(
                     color='b',
                     label='L2A original'
                 )
-                if idx == 0:
-                    axs[idx][jdx].title.set_text(f'ROI: {roi}') # use land-use information instead
-                if jdx == 0:
-                    axs[idx][jdx].set_ylabel(f'Reflectance {band} (%)')
+                axs[idx][jdx].yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
 
-        # TODO savefig
-        plt.show()
+                if idx == 0:
+                    axs[idx][jdx].title.set_text(roi_luc_mapping[roi])
+                if jdx == 0:
+                    axs[idx][jdx].set_ylabel(f'Reflectance {band} (%)', fontsize=28)
+
+            uncertainties.append(inner_band_dict)
+
+        # save plot
+        fname = f'{resolution}m_bands_l1c-l2a_refl-boxplots.png'
+        fig.savefig(out_dir.joinpath(fname), bbox_inches='tight')
+        plt.close(fig)
+    
+    # save the uncertainties per ROI into a dataframe and then to CSV
+    unc_df = pd.DataFrame(uncertainties)
+    unc_df.sort_values(by='band', ascending=True, inplace=True)
+    unc_df.to_csv(out_dir.joinpath('spectral_bands_l1c-l2a_unc.csv'), index=False)
 
 
 def unc_maps(
+        l1c_scenarios: str,
+        l2a_scenarios: str,
+        l1c_band_idx: int,
+        l2a_band_idx: int,
+        out_dir: Path
+    ) -> None:
+    """
+    Maps raster values of L1C and L2A uncertainty values to reveal
+    spatial pattern of uncertainty and their land cover/ use dependency
 
+    :param l1c_scenarios:
+        basename of tif files with L1C RUT output
+    :param l2a_scenarios:
+        basename of the atmospheric correction uncertainty output
+    :param l1c_band_idx:
+        L1C RUT band index
+    :param l2a_band_idx:
+        L2A atcorr uncertainty band index
+    """
+
+    # get files
+    l1c_scenarios = glob.glob(l1c_scenarios.as_posix())
+    l2a_scenarios = glob.glob(l2a_scenarios.as_posix())
+
+    # loop over bands
+    band_list = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B11', 'B12']
+
+    for band in band_list:
+
+        print(f'Working on band {band}')
+        
+        l2a_raster = [x for x in l2a_scenarios if Path(x).name.split('_')[1] == band][0]
+        l1c_raster = [
+            x for x in l1c_scenarios if Path(x).name.split('_')[-1].split('.')[0] == band.lower()
+        ][0]
+
+        # read rasters into arrays
+        with rio.open(l1c_raster, 'r') as src:
+            l1c_data = src.read(l1c_band_idx) * 0.1
+            meta = src.meta
+            bounds = src.bounds
+
+        with rio.open(l2a_raster, 'r') as src:
+            l2a_data = src.read(l2a_band_idx)
+            # since the S2-RUT has only 1 decimal precision, round
+            # the L2A data
+            l2a_data = np.round(l2a_data, 1)
+
+        single_fig, single_axs = plt.subplots(1, 2, figsize=(10,20))
+
+        epsg = meta['crs'].to_epsg()
+
+        # for colormap: find minimum of minima & maximum of maxima
+        minmin = math.trunc(np.min([np.min(l1c_data), np.min(l2a_data)]))
+        maxmax = np.round(np.max([np.max(l1c_data), np.max(l2a_data)]),0)
+
+        # cut values higher than 10%, otherwise there is not much to see in the L1C image
+        labelpad = 20
+        if maxmax > 10.:
+            maxmax = 10
+
+        # map
+        im_l1c = single_axs[0].imshow(
+            l1c_data, vmin=minmin, vmax=maxmax, cmap='bwr', interpolation='none',
+            extent=[bounds.left,bounds.right,bounds.bottom,bounds.top]
+        )
+        single_axs[0].title.set_text(f'L1C TOA {band}')
+        im_l2a = single_axs[1].imshow(
+            l2a_data, vmin=minmin, vmax=maxmax, cmap='bwr', interpolation='none',
+            extent=[bounds.left,bounds.right,bounds.bottom,bounds.top]
+        )
+        single_axs[1].title.set_text(f'L2A TOC {band}')
+
+        # add colormap: add_axes[left, bottom, width, heigth)
+        cbar_ax = single_fig.add_axes([0.92, 0.39, 0.04, 0.21])
+        n_ticks = int((maxmax - minmin) + 1)
+        v1 = np.linspace(minmin, maxmax, n_ticks, endpoint=True)
+        cbar_ticks_text = [int(i) for i in v1]
+        cbar_ticks_text[-1] = f'>{cbar_ticks_text[-1]}'
+        cbar = single_fig.colorbar(im_l2a, cax=cbar_ax)
+        cbar.ax.set_yticklabels(cbar_ticks_text)
+        cbar.set_label('Uncertainty [%] (k=1)', rotation=270, fontsize=16,
+                       labelpad=labelpad, y=0.45)
+
+        single_axs[0].set_xlabel(f'X [m] (EPSG:{epsg})', fontsize=14)
+        single_axs[1].set_xlabel(f'X [m] (EPSG:{epsg})', fontsize=14)
+        single_axs[0].set_ylabel(f'Y [m] (EPSG:{epsg})', fontsize=14)
+
+        single_axs[0].xaxis.set_ticks(np.arange(bounds.left, bounds.right, 5000))
+        single_axs[1].xaxis.set_ticks(np.arange(bounds.left, bounds.right, 5000))
+        single_axs[0].yaxis.set_ticks(np.arange(bounds.bottom, bounds.top, 5000))
+        single_axs[0].yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0f'))
+        single_axs[1].yaxis.set_ticks(np.arange(bounds.bottom, bounds.top, 5000))
+        single_axs[1].set_yticklabels([])
+
+        # save figure
+        fname = out_dir.joinpath(f'{band}_l1c-l2a_uncertainty-map.png')
+        single_fig.savefig(fname, bbox_inches='tight')
+        plt.close(single_fig)
+
+        del(cbar)
+        del(single_fig)
+
+
+def analyze_scl(
+        scl_scenarios: Path,
+        out_dir: Path
     ):
-    pass
-
-
+    """
+    Analyzes the differences in the scene classification layer among scenario
+    members to assses the impact of uncertainty on the scene classification
+    outcomes and hence the selection of "valid" pixels
+    """
+    
 
 
 
 if __name__ == '__main__':
 
+    # uncertainty maps
+    unc_res_raster_dir = Path('/mnt/ides/Lukas/owncloud/PhenomEn_Project/PAPER_1/S2A_MSIL2A_Analysis/S2B_MSIL1C_20190830T102029_N0208_R065_T32TMT_20190830T130621')
+    l1c_scenarios = unc_res_raster_dir.joinpath('*_rut_b*.tif')
+    l2a_scenarios = unc_res_raster_dir.joinpath('L2A_B*.tif')
+    l1c_band_idx = 1
+    l2a_band_idx = 5
+    out_dir = Path('/mnt/ides/Lukas/04_Work/Uncertainty/Atcorr')
+
+    unc_maps(l1c_scenarios, l2a_scenarios, l1c_band_idx, l2a_band_idx, out_dir)
+
     # box plots of ROIs (different landuses)
-    unc_res_roi_dir = Path('/run/media/graflu/ETH-KP-SSD6/SAT/S2A_MSIL2A_Analysis/S2B_MSIL1C_20190830T102029_N0208_R065_T32TMT_20190830T130621/ROIs')
+    unc_res_roi_dir = Path('/mnt/ides/Lukas/owncloud/PhenomEn_Project/PAPER_1/S2A_MSIL2A_Analysis/S2B_MSIL1C_20190830T102029_N0208_R065_T32TMT_20190830T130621/ROIs')
     l1c_roi_scenarios = unc_res_roi_dir.joinpath('L1C_*.csv')
     l2a_roi_scenarios = unc_res_roi_dir.joinpath('L2A_*.csv')
     l1c_original_data = unc_res_roi_dir.joinpath('*_MSIL1C_*.csv')
     l2a_original_data = unc_res_roi_dir.joinpath('*_MSIL2A_*.csv')
-    land_use_info = Path('')
+    land_use_info = Path('/home/graflu/public/Evaluation/Projects/KP0031_lgraf_PhenomEn/Uncertainty/STUDY_AREA/ZH_Polygons_2019_EPSG32632_samples_scenarios.shp')
     id_column = 'fid'
-    unc_boxplots(l1c_roi_scenarios, l2a_roi_scenarios, l1c_original_data, l2a_original_data, id_column, land_use_info)
+
+    unc_boxplots(
+        l1c_roi_scenarios=l1c_roi_scenarios,
+        l2a_roi_scenarios=l2a_roi_scenarios,
+        l1c_original_data=l1c_original_data,
+        l2a_original_data=l2a_original_data,
+        id_column=id_column,
+        land_use_info=land_use_info,
+        out_dir=out_dir
+    )
 
     # define inputs
     original_scene_l1c = Path(
@@ -452,5 +625,4 @@ if __name__ == '__main__':
         in_file_shp=in_file_shp,
         out_dir=out_dir,
         processing_level=processing_level
-    )
-    
+    )    

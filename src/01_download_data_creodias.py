420dc1a3e8941fd382c8d73f175ca3aaa9ab1964
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-10-28 08:14:35 +0200

    introduced main() fun to encapsulate code from user inputs

--- src/01_download_data_creodias.py
+++ src/01_download_data_creodias.py
@@ -1,42 +1,45 @@
 """
-This script shows how to download Sentinel-2 data
-from Creodias for a given region of interest, date range,
-cloud coverage and processing level.
 
-It requires an account at Creodias (https://creodias.eu/) and
-the account's username and password set in the environmental
-variables as (example for Win)
+@author:    Lukas Graf (D-USYS, ETHZ)
 
-SET CREODIAS_USER=<your-username>
-SET CREODIAS_PASSWORD0<your-password>
+@purpose:   This script shows how to download Sentinel-2 data
+            from Creodias for a given region of interest, date range,
+            cloud coverage and processing level.
+            
+            It requires an account at Creodias (https://creodias.eu/) and
+            the account's username and password set in the environmental
+            variables as (example for Win)
+            
+            SET CREODIAS_USER=<your-username>
+            SET CREODIAS_PASSWORD0<your-password>
+
+            It is the FIRST script in the uncertainty processing chain
 """
 
 
 import geopandas as gpd
 from datetime import date
+from pathlib import Path
+from typing import Optional
+
 from agrisatpy.downloader.sentinel2.creodias import query_creodias
 from agrisatpy.downloader.sentinel2.creodias import download_datasets
 from agrisatpy.downloader.sentinel2.creodias import ProcessingLevels
 
 
-if __name__ == '__main__':
-    
-    # define inputs
-    # processing level
-    processing_level = ProcessingLevels.L1C
-    # date range
-    start_date = date(2019,3,1)
-    end_date = date(2019,8,31)
-    # max_records defines the maximum number of datasets to download, increase if
-    # necessary; however, CREODIAS might impose a limitation...
-    max_records = 200
-
-    # filter by cloud cover (all scenes with a cloud cover lower than the threshold
-    # will be downloaded)
-    cloud_cover_threshold = 70
-
-    # shapefile defining the bounds of your region of interest
-    aoi_file = '/home/graflu/public/Evaluation/Projects/KP0031_lgraf_PhenomEn/02_Uncertainty/STUDY_AREA/AOI_Esch_EPSG32632.shp'
+def main(
+        start_date: date,
+        end_date: date,
+        processing_level: processing_level,
+        cloud_cover_threshold: int,
+        aoi_file: Path,
+        download_dir: Path,
+        max_records: Optional[int]=200
+    ) -> None:
+    """
+    the main executable function that calls all the required
+    processing steps
+    """
     bbox_data = gpd.read_file(aoi_file)
 
     # project to geographic coordinates (required for API query)
@@ -54,5 +57,39 @@ if __name__ == '__main__':
         cloud_cover_threshold=cloud_cover_threshold
     )
 
-    download_dir = '/run/media/graflu/ETH-KP-SSD6/SAT/S2A_MSIL1C_orig'
-    download_datasets(datasets, download_dir)
+    try:
+        download_datasets(datasets, download_dir)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    
+    ### user inputs
+    # processing level
+    processing_level = ProcessingLevels.L1C
+    # date range
+    start_date = date(2019,3,1)
+    end_date = date(2019,3,20)
+    # max_records defines the maximum number of datasets to download, increase if
+    # necessary; however, CREODIAS might impose a limitation...
+    max_records = 200
+
+    # filter by cloud cover (all scenes with a cloud cover lower than the threshold
+    # will be downloaded)
+    cloud_cover_threshold = 20
+
+    # shapefile defining the bounds of your region of interest
+    aoi_file = Path(
+        '/home/graflu/public/Evaluation/Projects/KP0031_lgraf_PhenomEn/Uncertainty/STUDY_AREA/AOI_Esch_EPSG32632.shp'
+    )
+    download_dir = Path('/run/media/graflu/ETH-KP-SSD6/SAT/S2A_MSIL1C_orig')
+
+    main(
+        start_date=start_date,
+        end_date=end_date,
+        processing_level=processing_level,
+        cloud_cover_threshold=cloud_cover_threshold,
+        aoi_file=aoi_file,
+        download_dir=download_dir
+    )

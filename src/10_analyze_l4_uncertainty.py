fcd6ba9071babb337a693845263d261664b97d3f
lukas <graflu@ethz.ch>
2022-01-04 10:15:18 +0100

    starting phenometric uncertainty assessment

---
+++ src/10_analyze_l4_uncertainty.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 4, 2022
+
+@author: graflu
+'''
+
+from pathlib import Path
+
+
+def calc_l4_uncertainty(
+        uncertainty_dir: Path
+    ):
+    
+if __name__ == '__main__':
+    
+
+    uncertainty_dir = Path('../S2_TimeSeries_Analysis')
